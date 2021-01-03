e06c18cb881046e7d11dcdf3a8b73f0cfea8d9b3
abdelayiti <abdelhay.courses@gmail.com>
2021-01-03 16:43:05 +0200

    abdelhay master first commit

---
+++ Calc.java
@@ -0,0 +1,36 @@
+public class Calc{
+	public static void main(String[] args){
+			
+		int index= args.length;
+		if(index < 3 || index > 3){
+			System.out.println("please enter 3 arguments");
+		}else{
+			int operand1= Integer.parseInt(args[0]);
+			int operand2= Integer.parseInt(args[2]);
+			
+			if(args[1].equals("+")){
+				System.out.print(args[0] + " + " +args[2] + " = ");
+				System.out.print((operand1+operand2));
+			
+			}else if(args[1].equals("-")){
+				System.out.print(args[0] + " - " +args[2] + " = ");
+				System.out.print((operand1-operand2));
+				
+			}else if(args[1].equals("*")){
+				System.out.print(args[0] + " * " +args[2] + " = ");
+				System.out.print((operand1*operand2));
+			
+			}else if(args[1].equals("/")){
+				if(operand2 >1 && operand1>operand2){
+				System.out.print(args[0] + " / " +args[2] + " = ");
+				System.out.print((operand1/operand2));
+				}else{
+					System.out.println("make operand2 > operand1");
+				}
+				
+			}else{
+				System.out.println("wrong operation");
+			}
+		}
+	}
+}
